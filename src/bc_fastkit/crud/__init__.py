9fdd46b7e8665f43f4bc34dc6060ed6938ee8bcd
alian <salivawbc@gmail.com>
2026-01-19 15:59:53 +0800

    feat: update

--- src/bc_fastkit/crud/__init__.py
+++ src/bc_fastkit/crud/__init__.py
@@ -1,4 +1,4 @@
-from .base import CRUDBase
+from .base import CRUDBase, CRUDHookMixin
 from .core.typing import ModelType
 
-__all__ = ["CRUDBase", "ModelType"]
+__all__ = ["CRUDBase", "ModelType", "CRUDHookMixin"]
